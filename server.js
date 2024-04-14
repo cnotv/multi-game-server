79eadddef682a4b26f329a4b5f018ea1c028cefc
cnotv <giuseppe.leo@suse.com>
2024-04-15 01:17:28 +0200

    Set initial communications for the chat and user creation

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ const io = new Server(httpServer, {
     origin: ['https://cnotv-multi-game.netlify.app/', 'http://localhost:4000/'],
     methods: ['GET', 'POST'],
   },
- });
+});
+ 
+let users = [];
+const messages = [];
 
 io.on('connection', (socket) => {
   const count = io.engine.clientsCount;
@@ -29,6 +32,37 @@ io.on('connection', (socket) => {
   }
 
   console.log(`New connection: ${socket.id} - Total clients: ${count}`);
+
+
+  socket.on('user:create', (user) => {
+    console.log('user:create', user);
+    users.push(user);
+    io.emit('user:created', users);
+  });
+  
+  socket.on('user:change', (user) => {
+    console.log('user:change', user);
+    users = [
+      ...users.filter(u => u.id !== user.id),
+      user
+    ];
+    io.emit('user:created', users);
+  });
+
+  socket.on('user:created', (user) => {
+    console.log('user:created', user);
+  });
+
+  socket.on('message:create', (message) => {
+    console.log('message:create', message);
+    messages.push(message);
+    io.emit('message:created', message);
+  });
+
+  socket.on('message:created', (message) => {
+    console.log('message:created', message);
+  });
+
 
   io.engine.on('connection_error', (err) => {
     console.log(err.message);
